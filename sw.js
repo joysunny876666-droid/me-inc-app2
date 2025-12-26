beef0fcaad9b54bef13cef37cd0c09350d861ad1
joysunny <joysunny876666@gmail.com>
2025-12-26 13:02:39 +0800

    Bump SW version to force update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'me-inc-v2';
+const CACHE_NAME = 'me-inc-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -11,6 +11,18 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Clean up old caches
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
     );
 });
 
